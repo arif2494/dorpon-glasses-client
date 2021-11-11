9372d346c12a7df4f2dba5db9e4694f5c7463c2e
Ariful Islam <arifulislam161024@gmail.com>
2021-11-11 10:18:45 +0600

    load reviews from server

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,7 +3,7 @@ import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
 import OverView from '../OverView/OverView';
 import Products from '../Products/Products';
-import Review from '../Review/Review';
+import Reviews from '../Reviews/Reviews';
 import TopBanner from '../TopBanner/TopBanner';
 
 const Home = () => {
@@ -13,7 +13,7 @@ const Home = () => {
 			<TopBanner />
 			<OverView />
 			<Products />
-			<Review />
+			<Reviews />
 			<Footer />
 		</div>
 	);
---
+++ src/Pages/Home/Reviews/Reviews.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { Swiper } from 'swiper/react/swiper';
+import { SwiperSlide } from 'swiper/react/swiper-slide';
+import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper';
+// Import Swiper styles
+import './swiper.min.css';
+import './pagination.min.css';
+import './navigation.min.css';
+import './effect-fade.min.css';
+import './review.css';
+import man from '../../../img/icon/man.png';
+const Reviews = () => {
+	const [ reviews, setReviews ] = useState([]);
+	useEffect(() => {
+		fetch('http://localhost:5000/reviews').then((res) => res.json()).then((data) => setReviews(data));
+	}, []);
+	console.log(reviews, 'review');
+	return (
+		<div>
+			<h1 className=" text-center my-3 text-3xl font-medium">What Customer Says About Us</h1>
+			<Swiper
+				modules={[ Navigation, Pagination, Autoplay, EffectFade ]}
+				spaceBetween={0}
+				slidesPerView={1}
+				loop={true}
+				effect={'fade'}
+				centeredSlides={true}
+				autoplay={{
+					delay: 3000,
+					disableOnInteraction: true
+				}}
+				style={{ height: '300px' }}
+				navigation
+				pagination={{
+					type: 'fraction'
+				}}
+			>
+				{reviews.map((review) => (
+					<SwiperSlide>
+						<img src={man} className="icon-width" alt="!" />
+						<h1 className="text-2xl my-2 font-medium">{review.name}</h1>
+						<p className="w-9/12 font-medium text-gray-500">{review.review}</p>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+};
+
+export default Reviews;
