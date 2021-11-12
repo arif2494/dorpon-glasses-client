cecc52738e5eb4bcef80c06437304b1a2d654585
Ariful Islam <arifulislam161024@gmail.com>
2021-11-12 17:11:56 +0600

    manage order for admin with order status done

--- src/Pages/DashBoard/ManageOrders/ManageOrders.js
+++ src/Pages/DashBoard/ManageOrders/ManageOrders.js
@@ -1,9 +1,148 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 const ManageOrders = () => {
+	const [ orders, setOrders ] = useState([]);
+	useEffect(() => {
+		fetch('http://localhost:5000/orders').then((res) => res.json()).then((data) => setOrders(data));
+	}, []);
+	// ship an order
+	const shippedOrder = (id) => {
+		const uri = `http://localhost:5000/order/${id}`;
+		fetch(uri, {
+			method: 'PUT'
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.modifiedCount > 0) {
+					alert('Order Shipped');
+					window.location.reload();
+				}
+			});
+	};
+	// cancel an order
+	const handleCancelOrder = (id) => {
+		const url = `http://localhost:5000/order/cancel/${id}`;
+		fetch(url, {
+			method: 'DELETE'
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.deletedCount > 0) {
+					alert('Order Cancelled');
+					window.location.reload();
+				}
+			});
+	};
 	return (
 		<div>
-			<h1>Manag orders</h1>
+			<div className="my-6">
+				<h1 className="text-center text-3xl font-thin">Manage Orders</h1>
+			</div>
+			{/* sgow all orders */}
+			<div>
+				<div className="container mx-auto">
+					<div className="flex flex-col">
+						<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+							<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+								<div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+									<table className="min-w-full divide-y divide-gray-200">
+										<thead className="bg-gray-50">
+											<tr>
+												<th
+													scope="col"
+													className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>
+													Name
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>
+													Title & Address
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>
+													Status
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>
+													Price
+												</th>
+												<th
+													scope="col"
+													className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+												>
+													Cancel
+												</th>
+											</tr>
+										</thead>
+										<tbody className="bg-white divide-y divide-gray-200">
+											{orders.map((order) => (
+												<tr key={order._id}>
+													<td className="px-6 py-4 whitespace-nowrap">
+														<div className="flex items-center">
+															<div className="flex-shrink-0 h-10 w-10">
+																<img
+																	className="h-10 w-10 rounded-full"
+																	src={order.img}
+																	alt=""
+																/>
+															</div>
+															<div className="ml-4">
+																<div className="text-sm font-medium text-gray-900">
+																	{order.name}
+																</div>
+																<div className="text-sm text-gray-500">
+																	{order.email}
+																</div>
+															</div>
+														</div>
+													</td>
+													<td className="px-6 py-4 whitespace-nowrap">
+														<div className="text-sm text-gray-900">{order.productName}</div>
+														<div className="text-sm text-gray-500">{order.address}</div>
+													</td>
+													<td className="px-6 py-4 whitespace-nowrap">
+														{order.status ? (
+															<button
+																onClick={() => shippedOrder(order._id)}
+																className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-green-800"
+															>
+																{order.status ? order.status : 'Pending'}
+															</button>
+														) : (
+															<button
+																onClick={() => shippedOrder(order._id)}
+																className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-green-800"
+															>
+																{order.status ? order.status : 'Pending'}
+															</button>
+														)}
+													</td>
+													<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+														{order.productPrice}
+													</td>
+													<td className="px-6 py-4 whitespace-nowrap">
+														<button
+															onClick={() => handleCancelOrder(order._id)}
+															className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-green-800"
+														>
+															Cancel
+														</button>
+													</td>
+												</tr>
+											))}
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
